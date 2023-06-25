27c2f1c0429927045f44a86a95020f20fcf3e753
Sellakumar <sellakumark@outlook.com>
2023-06-25 16:07:07 +0530

    mail service API integrated

--- script/careers.js
+++ script/careers.js
@@ -1,29 +1,17 @@
-$('.checkout-contact-form').submit(startExternalPayment);
-function startExternalPayment(e) {
-    e.preventDefault();
-    var name = $('.checkout-contact-form .cus-name').val();
-    var email = $('.checkout-contact-form .cus-email').val();
-    var phone = $('.checkout-contact-form .cus-phone').val();
-	var message= $('.checkout-contact-form .cus-msg').val();
-	var file = $('.checkout-contact-form .cus-atc').val();
-	
-    var ticket = JSON.parse(localStorage.getItem("ticketDetails"));
+function onSubmitForm() {
     var formDetails = {
-   name: 'name',
-   email: 'email',
-   phone: 'phone',
-   message: 'message',
-   file: 'file'
-};
-var url = `https://mail-service.vercel.app/api/email/`;
-var http = new XMLHttpRequest();
-http.open("POST", url, true);
-http.setRequestHeader("Accept", "application/json");
-http.setRequestHeader("Content-type", "application/json");
-http.onreadystatechange = function () {
-    if (this.readyState === this.DONE && this.status === 200) {
-       // success response
-    }
-};
-http.send(JSON.stringify(formDetails));
+        name: document.querySelector('.careers-form .cus-name').value,
+        email: document.querySelector('.careers-form .cus-email').value,
+        phone: document.querySelector('.careers-form .cus-phone').value,
+        message: document.querySelector('.careers-form .cus-msg').value,
+        file: document.querySelector('.careers-form .cus-atc').value
+    };
+    var http = new XMLHttpRequest();
+    http.open('POST', 'https://mail-service-c63p3wpkl-ssivakumar0407.vercel.app/api/email/', true);
+    http.onreadystatechange = function () {
+        if (this.readyState === this.DONE && this.status === 200) {
+            // success response
+        }
+    };
+    http.send(JSON.stringify(formDetails));
 }
