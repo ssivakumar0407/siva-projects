02ac135e2092b7b1f427dad60a0f200a635af55d
Sellakumar <sellakumark@outlook.com>
2023-06-29 21:57:43 +0530

    mail service attachment changes committed

--- script/careers.js
+++ script/careers.js
@@ -4,10 +4,12 @@ function onSubmitForm() {
         email: document.querySelector('.careers-form .cus-email').value,
         phone: document.querySelector('.careers-form .cus-phone').value,
         message: document.querySelector('.careers-form .cus-msg').value,
-        file: document.querySelector('.careers-form .cus-atc').value
+        file: selectedFile
     };
     var http = new XMLHttpRequest();
     http.open('POST', 'https://mail-service-c63p3wpkl-ssivakumar0407.vercel.app/api/email/', true);
+    http.setRequestHeader("Accept", "application/json");
+    http.setRequestHeader("Content-type", "application/json");
     http.onreadystatechange = function () {
         if (this.readyState === this.DONE && this.status === 200) {
             // success response
@@ -15,3 +17,19 @@ function onSubmitForm() {
     };
     http.send(JSON.stringify(formDetails));
 }
+
+var selectedFile = [];
+
+document.addEventListener("DOMContentLoaded", function () {
+    const fileInput = document.querySelector('.careers-form .cus-atc');
+    fileInput.onchange = () => {
+        selectedFile = [];
+        for (var a = 0; a < fileInput.files.length; a++) {
+            var reader = new FileReader();
+            reader.onload = (evt) => {
+                selectedFile.push(evt.target.result);
+            };
+            reader.readAsText(fileInput.files[a]);
+        }
+    }
+});
